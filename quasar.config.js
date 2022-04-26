374c6da7e5ce86ed08ae8915e884ef05e217dc3e
Nuno Góis <github@nunogois.com>
2022-04-26 17:30:12 +0100

    cleanup

--- quasar.config.js
+++ quasar.config.js
@@ -28,7 +28,7 @@ module.exports = configure(function (/* ctx */) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: ['axios', 'i18n', 'supabase'],
+    boot: ['i18n', 'supabase'],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
     css: ['app.sass'],
